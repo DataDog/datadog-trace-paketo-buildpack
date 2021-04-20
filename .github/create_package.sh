1536e3f795a4d2b1b1283094a6288e15dd0279ef
kevin.feng <kevin.feng@datadoghq.com>
2021-04-20 13:38:49 -0400

    change test.yml to run scripts

---
+++ .github/create_package.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir ${HOME}/build
+
+echo "Package repo using jam"
+jam pack --buildpack "${GITHUB_WORKSPACE}/buildpack.toml" --version ${BUILDPACK_VERSION} --output "${HOME}/build/${PACKAGE_NAME}"
+
+echo "Building docker image using packaged tgz"
+echo "[buildpack]
+uri = '${HOME}/build/${PACKAGE_NAME}'
+[platform]
+os = 'linux'" > ${HOME}/build/package.toml
+pack package-buildpack "${BUILDPACK_NAME}:${BUILDPACK_VERSION}" --config ${HOME}/build/package.toml --format image
---
+++ .github/install_dependencies.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Installing jam ${JAM_VERSION}"
+
+mkdir -p "${HOME}"/bin
+
+echo "${HOME}/bin" >> "${GITHUB_PATH}"
+curl \
+    --location \
+    --show-error \
+    --silent \
+    "https://github.com/paketo-buildpacks/packit/releases/download/v${JAM_VERSION}/jam-linux" \
+    --output ${HOME}/bin/jam
+
+chmod a+x "${HOME}"/bin/jam
+
+echo "Installing pack ${PACK_VERSION}"
+curl \
+    --location \
+    --show-error \
+    --silent \
+    "https://github.com/buildpacks/pack/releases/download/v${PACK_VERSION}/pack-v${PACK_VERSION}-linux.tgz" \
+| tar -C "${HOME}"/bin -xz pack
